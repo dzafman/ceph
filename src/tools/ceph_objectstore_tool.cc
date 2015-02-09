c9328479913609d39af91f794c8d965ebbde1a44
David Zafman <dzafman@redhat.com>
2015-02-09 15:37:57 -0800

    ceph-objectstore-tool: Check pgid validity earlier like we did before
    This code was not doing the umount() even though it was moved after mount()
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -2500,6 +2500,11 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  if (pgidstr.length() && !pgid.parse(pgidstr.c_str())) {
+    cerr << "Invalid pgid '" << pgidstr << "' specified" << std::endl;
+    return 1;
+  }
+
   ObjectStore *fs = ObjectStore::create(g_ceph_context, type, dpath, jpath, flags);
   if (fs == NULL) {
     cerr << "Must provide --type (filestore, memstore, keyvaluestore)" << std::endl;
@@ -2561,11 +2566,6 @@ int main(int argc, char **argv)
       << superblock.compat_features << std::endl;
     ret = EINVAL;
     goto out;
-  }
-
-  if (pgidstr.length() && !pgid.parse(pgidstr.c_str())) {
-    cerr << "Invalid pgid '" << pgidstr << "' specified" << std::endl;
-    return 1;
   }
 
   if (op != "list" && vm.count("object")) {
