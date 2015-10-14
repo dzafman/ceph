cf23d0764d544055ab53508461c3d09b68e3ce9b
David Zafman <dzafman@redhat.com>
2015-10-13 18:02:32 -0700

    osd: Additional _scrub() checks for snapset inconsistency
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -12211,6 +12211,11 @@ void ReplicatedPG::_scrub(
 
 	if (!snapset.get().clones.empty()) {
 	  dout(20) << "  snapset " << snapset.get() << dendl;
+	  if (snapset.get().seq == 0) {
+	    osd->clog->error() << mode << " " << info.pgid << " " << soid
+			       << " snaps.seq not set";
+	    ++scrubber.shallow_errors;
+          }
 	}
 
 	if (soid.is_head() && !snapset.get().head_exists) {
