daf7090a2233271501c26dde6f56566952c81561
David Zafman <dzafman@redhat.com>
2020-01-15 01:17:24 +0000

    Core diagnostics

---
+++ qa/standalone/core_test/test-failure.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -ex
+
+source $CEPH_ROOT/qa/standalone/ceph-helpers.sh
+
+function run() {
+    local dir=$1
+    shift
+
+    export CEPH_MON="127.0.0.1:7202" # git grep '\<7202\>' : there must be only one
+    export CEPH_ARGS
+    CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
+    CEPH_ARGS+="--mon-host=$CEPH_MON "
+
+    local funcs=${@:-$(set | sed -n -e 's/^\(TEST_[0-9a-z_]*\) .*/\1/p')}
+    for func in $funcs ; do
+        setup $dir || return 1
+        $func $dir || return 1
+        teardown $dir || return 1
+    done
+}
+
+function TEST_failure_core_only() {
+    local dir=$1
+
+    run_mon $dir a || return 1
+    sleep 20
+    kill_daemons $dir SEGV mon 5
+    sleep 10
+    return 0
+}
+
+main test_failure "$@"
